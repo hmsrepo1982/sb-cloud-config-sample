f511e9a4a7f92e00330818588e4c722e2890a810
hmsrepo1982 <54878591+hmsrepo1982@users.noreply.github.com>
2020-03-29 21:44:22 -0500

    Add files via upload

---
+++ src/test/java/com/hms/sbcloudconfigsample/SbCloudConfigSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.sbcloudconfigsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCloudConfigSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
